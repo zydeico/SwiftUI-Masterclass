80b7f84dd4e3a8035f04bf10606a6a05210cce42
Daniel <daniel.prolite@gmail.com>
2020-02-15 10:44:45 -0600

    Initial Commit

---
+++ SwiftUI Course/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Course
+//
+//  Created by Daniel Vázquez on 15/02/20.
+//  Copyright © 2020 Daniel Vázquez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
