3127465319e787589cedd263c2fab897d0ac92ee
Daniel <daniel.prolite@gmail.com>
2020-02-26 21:11:02 -0600

    Animations added

--- SwiftUI Course/ViewController/CardViewController.swift
+++ SwiftUI Course/ViewController/CardViewController.swift
@@ -13,11 +13,17 @@ struct CardView: View {
     // MARK: - PROPERTIES
     var card: Card
     
+    @State private var fadeIn: Bool = false
+    @State private var moveDownWard: Bool = false
+    @State private var moveUpWard: Bool = false
+    
     // Mark: - Card
     
     var body: some View {
         ZStack {
             Image(card.imageName)
+                .opacity(fadeIn ? 1.0 : 0.0)
+            
             VStack {
                 Text("SwiftUI")
                     .font(.largeTitle)
@@ -26,11 +32,11 @@ struct CardView: View {
                 Text(card.headline)
                     .fontWeight(.light)
                     .foregroundColor(Color.white)
-                .italic()
+                    .italic()
             }
                 
-            // Texto en el top de la tarjeta
-            .offset(y: -218)
+                // Texto en el top de la tarjeta
+                .offset(y: moveDownWard ? -218 : -300)
             
             // Adding button
             Button(action: {
@@ -51,15 +57,25 @@ struct CardView: View {
                 .padding(.vertical)
                 .padding(.horizontal, 24)
                 .background(LinearGradient(gradient: Gradient(colors: card.gradientColors), startPoint: .leading, endPoint: .trailing))
-                    .clipShape(Capsule())
+                .clipShape(Capsule())
                 .shadow(color: Color("ColorShadow"), radius: 6, x: 0, y: 3)
             }
-            .offset(y: 210)
+            .offset(y: moveUpWard ? 210 : 300)
         }
+            
         .frame(width: 335, height: 545)
         .background(LinearGradient(gradient: Gradient(colors: card.gradientColors), startPoint: .top, endPoint: .bottom))
         .cornerRadius(16)
         .shadow(radius: 8)
+        .onAppear() {
+            withAnimation(.linear(duration: 1.2)) {
+                self.fadeIn.toggle()
+            }
+            withAnimation(.linear(duration: 0.8)) {
+                self.moveDownWard.toggle()
+                self.moveUpWard.toggle()
+            }
+        }
     }
 }
 
